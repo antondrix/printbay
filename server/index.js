cc85d8923e3c430e9e8696c5392299b6c1916055
Anton Vasilev <a.vasilev@gaijin.team>
2022-11-12 12:29:24 +0300

    Setup development environment

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+require("dotenv").config({
+  path: path.join(__dirname, "..", ".env.server")
+});
 
-const port = 8070;
+const port = process.env.PORT
+;
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening gracefully on port ${port}`);
 });
